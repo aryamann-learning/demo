f8b1e0117e6f691fd499c3902d2b1ebc90205515
SYS <arya00014@gmail.com>
2021-10-07 13:19:45 +0530

    user API'S

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,53 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+	private int id;
+    private String firstName;
+    private String lastName;
+
+    public User() {
+    }
+
+    public User(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+//other setters and getters
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setId(Integer id2) {
+		this.id=id2;
+		
+	}
+
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,34 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+@Service
+@Transactional
+public class UserService {
+	@Autowired
+    private UserRepository userRepository;
+    public List<User> listAllUser() {
+        return userRepository.findAll();
+    }
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+    public void saveUsers(List<User> users) {
+        userRepository.saveAll(users);
+    }
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
+    public void deleteUser(Integer id) {
+        userRepository.deleteById(id);
+    }
+}
